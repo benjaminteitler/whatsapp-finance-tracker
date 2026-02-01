910f6044bedf848751045c7d0fbe981476919e1c
benjamin-teitler <benjamin.teitler@mail.huji.ac.il>
2026-02-01 23:43:56 +0200

    Add explicit WhatsApp bot logging and QR code output

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -2,11 +2,17 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const { GROUP_ID } = require('./config');
 const { handleMessage } = require('./handler');
 
+console.log('Starting WhatsApp client...');
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
+});
+
+client.on('qr', (qr) => {
+  console.log('Scan this QR code with WhatsApp:');
+  console.log(qr);
 });
 
 client.on('message', async (msg) => {
@@ -17,6 +23,14 @@ client.on('message', async (msg) => {
 
 client.on('ready', () => {
   console.log('WhatsApp client is ready.');
+});
+
+client.on('auth_failure', (msg) => {
+  console.error('AUTHENTICATION FAILURE:', msg);
+});
+
+client.on('disconnected', (reason) => {
+  console.error('WhatsApp client disconnected:', reason);
 });
 
 module.exports = client;
